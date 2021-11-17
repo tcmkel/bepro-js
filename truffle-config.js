641a96617b138307771ca0ec62b374d0cc9eaac7
tcmkel <89959768+tcmkel@users.noreply.github.com>
2021-11-17 12:21:11 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,8 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
+      gas: 4500000,
+      gasPrice:0
       network_id: "*" // Match any network id
     },
     leprichain: {
